d2808b9f5230927dc97e6adf4ff46607307875de
tduval-xtensible <tduval@xtensible.net>
2022-11-24 12:40:06 -0500

    :art: output pretty n3

--- vscode-extension-n3-execute/src/n3/format_results.py
+++ vscode-extension-n3-execute/src/n3/format_results.py
@@ -1,7 +1,7 @@
 import sys
 from rdflib import Graph
 
-# g = Graph()
-# g.parse(data=str(sys.argv[2]), format='n3')
-# print(g.serialize(format='n3'))
-print('hello world')
+g = Graph()
+g.parse(data=str(sys.argv[2]), format='n3')
+print(g.serialize(format='n3'))
+
